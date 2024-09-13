7c00a8d39db86b3ff89feb700f696fc3a7bb10c2
José Elias Herminio Mendes <jose.ehmendes@senacsp.edu.br>
2024-09-13 18:30:22 -0300

    Atualizando o exercício 5

--- ex5/script.js
+++ ex5/script.js
@@ -5,17 +5,43 @@ números, considere as premissas abaixo:
 • Crie um menu que o usuário escolha cadastrar 5, 15, 25 ou 50.
 • Ao final do cadastro ele tem um outro menu pra escolher exibir:
 • A lista cadastrada, somente os pares, ou somente os impares. */
-
-// Variáveis usadas
 let numeros = [];
 let pares = [];
 let impares = [];
 
-executarAlgoritmo();
-
-function executarAlgoritmo() {
-    let mensagem = "";
+principal5();
+function principal5() {
     let option = menudeNumeros();
+    if (processarCadastro(option)) { 
+        return;
+    }
+    separacaoNumeros(numeros);
+    let exibicao = menudeExibicao();
+    if (processarExibicao(exibicao)) {
+        return;
+    }
+}
+ 
+function menudeNumeros() {
+    let opcao;
+    let infoMenu = `Informe quantos números você deseja cadastrar:
+1. Cinco números.
+2. Quinze números.
+3. Vinte e cinco números.
+4. Cinquenta números.
+5. Sair.`;
+    
+    do {
+        opcao = prompt(infoMenu);
+        if (opcao !== "1" && opcao !== "2" && opcao !== "3" && opcao !== "4" && opcao !== "5") {
+            alert("Opção inválida. Por favor, escolha um número entre 1 e 5.");
+        }
+    } while (opcao !== "1" && opcao !== "2" && opcao !== "3" && opcao !== "4" && opcao !== "5");
+    
+    return opcao;
+}
+
+function processarCadastro(option) {
     switch (option) {
         case "1":
             lerNumeros(5);
@@ -30,40 +56,13 @@ function executarAlgoritmo() {
             lerNumeros(50);
             break;
         case "5":
-            return;
+            alert("Você escolheu sair do algoritmo.");
+            return true;
         default:
-            mensagem = "Operação inválida. Por favor, escolha um número entre 1 e 5.";
-            alert(mensagem);
-            return executarAlgoritmo();
+            alert("Operação inválida. Por favor, escolha um número entre 1 e 5.");
+            principal5();
     }
-    separacaoNumeros(numeros);
-    let exibicao = menudeExibicao();
-    switch (exibicao) {
-        case "1":
-            exibirCompleto(numeros);
-            break;
-        case "2":
-            exibirPares(pares);
-            break;
-        case "3":
-            exibirImpares(impares);
-            break;
-        default:
-            mensagem = "Operação inválida. Por favor, escolha um número entre 1 e 3.";
-            alert(mensagem);
-            return executarAlgoritmo();
-    }
-}
-
-function menudeNumeros() {
-    let infoMenu = `Informe quantos números você deseja cadastrar:
-1. Cinco números.
-2. Quinze números.
-3. Vinte e cinco números.
-4. Cinquenta números.
-5. Sair.`;
-    let opcao = prompt(infoMenu);
-    return opcao;
+    return false;
 }
 
 function lerNumeros(quantidade) {
@@ -89,6 +88,8 @@ function lerNumeros(quantidade) {
 }
 
 function separacaoNumeros(numeros) {
+    pares = [];
+    impares = [];
     for (let i = 0; i < numeros.length; i++) {
         if (numeros[i] % 2 === 0) {
             pares.push(numeros[i]);
@@ -99,49 +100,65 @@ function separacaoNumeros(numeros) {
 }
 
 function menudeExibicao() {
-    let infoMenu = `Informe os números você deseja exibir:
+    let exibicao;
+    let infoMenu = `Informe o que você deseja exibir:
 1. Todos os números digitados.
 2. Somente os números Pares.
-3. Somente os números Ímpares.`;
-    let exibicao = prompt(infoMenu);
+3. Somente os números Ímpares.
+4. Mudei de ideia, não quero usar o algoritmo.`;
+    
+    do {
+        exibicao = prompt(infoMenu);
+        if (exibicao !== "1" && exibicao !== "2" && exibicao !== "3" && exibicao !== "4") {
+            alert("Opção inválida. Por favor, escolha um número entre 1 e 4.");
+        }
+    } while (exibicao !== "1" && exibicao !== "2" && exibicao !== "3" && exibicao !== "4");
+    
     return exibicao;
+}
+
+function processarExibicao(exibicao) {
+    switch (exibicao) {
+        case "1":
+            exibirCompleto(numeros);
+            break;
+        case "2":
+            exibirPares(pares);
+            break;
+        case "3":
+            exibirImpares(impares);
+            break;
+        case "4":
+            alert("Você escolheu sair do algoritmo.");
+            return true;
+        default:
+            alert("Operação inválida. Por favor, escolha um número entre 1 e 4.");
+            principal5();
+    }
+    return false;
 }
 
 function exibirCompleto(numeros) {
     let mensagem = `Os números que você digitou foram: \n`;
     for (let i = 0; i < numeros.length; i++) {
-        if (i < numeros.length - 1) {
-            mensagem += `${numeros[i]}, `;
-        } else {
-            mensagem += `${numeros[i]}.`;
-        }
+        mensagem += (i < numeros.length - 1) ? `${numeros[i]}, ` : `${numeros[i]}.`;
     }
     alert(mensagem);
-    return;
 }
 
 function exibirPares(pares) {
     let mensagem = `Os números pares que você digitou foram: \n`;
     for (let i = 0; i < pares.length; i++) {
-        if (i < pares.length - 1) {
-            mensagem += `${pares[i]}, `;
-        } else {
-            mensagem += `${pares[i]}.`;
-        }
+        mensagem += (i < pares.length - 1) ? `${pares[i]}, ` : `${pares[i]}.`;
     }
     alert(mensagem);
-    return;
 }
 
 function exibirImpares(impares) {
     let mensagem = `Os números ímpares que você digitou foram: \n`;
     for (let i = 0; i < impares.length; i++) {
-        if (i < impares.length - 1) {
-            mensagem += `${impares[i]}, `;
-        } else {
-            mensagem += `${impares[i]}.`;
-        }
+        mensagem += (i < impares.length - 1) ? `${impares[i]}, ` : `${impares[i]}.`;
     }
     alert(mensagem);
-    return;
 }
+
